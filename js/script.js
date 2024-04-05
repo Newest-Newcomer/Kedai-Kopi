b844982b91806f6c124006c260c758e3225a6fe8
Ishak Panjaitan <60592577+Newest-Newcomer@users.noreply.github.com>
2024-04-05 20:41:22 +0700

    Menambahkan Search Form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-// Toogle class active
+// Toggle class active untuk hamburger menu.
 const navbarNav = document.querySelector(".navbar-nav");
-// Ketika hamburger menu di klik
+// Ketika hamburger menu di klik maka akan ditambahkan class active pada id #hamburger-menu.
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toggle class active untuk search form.
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+// Untuk menampilakan .search-form ketika #search-button di klik.
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  // Untuk mengautomasi klik pada #search-box
+  searchBox.focus();
+  // Untuk mencegah pada saat #search-box di klik maka kembali bagian paling atas halaman(jika href="#").
+  e.preventDefault();
+};
+
 // Side bar tertutup ketika klik diluar side bar.
-const hamburger = document.querySelector("#hamburger-menu");
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
   // Cara bacanya, jika e.target tidak ada didalam const hamburger dan tidak ada didalam const navbarNav maka.
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  // Untuk menghilangkan .search-form ketika di klik diluar dari #search-button dan .search-form.
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
