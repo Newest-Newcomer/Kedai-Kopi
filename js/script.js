d7c2049fadba899a3cec0ad7326a6597a86e3837
Ishak Panjaitan <60592577+Newest-Newcomer@users.noreply.github.com>
2024-04-08 14:26:19 +0700

    Update 0.2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // Toggle class active untuk hamburger menu.
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di klik maka akan ditambahkan class active pada id #hamburger-menu.
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Toggle class active untuk search form.
@@ -17,10 +18,18 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
-// Side bar tertutup ketika klik diluar side bar.
+// Toggle class active untuk shopping cart list
+const shoppingCart = document.querySelector(".shopping-cart-list");
+document.querySelector("#cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#cart-button");
 
+// Side bar tertutup ketika klik diluar side bar.
 document.addEventListener("click", function (e) {
   // Cara bacanya, jika e.target tidak ada didalam const hamburger dan tidak ada didalam const navbarNav maka.
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -31,4 +40,33 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+
+  // Untuk mengilangkan .shopping-cart-list ketika di klik diluar dari #cart-button dan .shopping-cart-list.
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Open Modal Box (Detail Produk)
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const detailButtons = document.querySelectorAll(".detail-button");
+
+detailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Untuk meng-close .#item-detail-modal ketika di klik diluar dari #item-detail-modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
+
+// Close Button Modal Box
+document.querySelector(".modal .detail-close").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
